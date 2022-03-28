20e2f88c54700473410de4872295f5b2451dd7b7
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2022-03-27 20:41:53 -0600

    Game mechanism

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,9 +9,12 @@ import { FlagService } from '../flag.service';
 })
 export class GameComponent implements OnInit {
   flags: Flag[] = [];
+  totalFlags: number = 51;
   countryName: string = '';
   countryPath: string = '';
   playerGuess: string = '';
+  stateMessage: string = 'Write the name';
+  random: number = 0;
 
   constructor(private flagService: FlagService) { }
 
@@ -24,11 +27,22 @@ export class GameComponent implements OnInit {
   }
 
   nextFlag(): void {
-    let random = Math.floor(Math.random() * this.flags.length);
-    this.countryName = this.flags[random].name;
-    this.countryPath = this.flags[random].path;
+    this.random = Math.floor(Math.random() * this.flags.length);
+    this.countryName = this.flags[this.random].name;
+    this.countryPath = this.flags[this.random].path;
     this.playerGuess = '';
     console.log(this.countryName, this.countryPath);
+  }
+
+  validateChoice(): void {
+    if (this.playerGuess && this.playerGuess == this.countryName) {
+      this.stateMessage = 'Correct Guess!';
+      this.flags.splice(this.random, 1);
+      this.totalFlags = this.flags.length;
+      this.nextFlag();
+    } else {
+      this.stateMessage = 'Incorrect Guess!';
+    }
   }
 
 }
